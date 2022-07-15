a62c355c04048b87b8a2ca981ea7cbf8a72dfe7e
huseyinkarpuzcu1 <huseyinkarpuzcu371@gmail.com>
2022-07-15 10:13:26 +0300

    Add project files.

---
+++ MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void musteriEkle(Musteri name)
+        {
+            Console.WriteLine(name.Name+"      eklendi");
+            Console.WriteLine("-------------------------------------------------");
+
+        }
+        public void musteriSil(Musteri name)
+        {
+            Console.WriteLine(name.Name + "      silindi");
+            Console.WriteLine("-------------------------------------------------");
+        }
+        public void musteriListele(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Listele : " + musteri.Name + " " + musteri.Surname);
+            Console.WriteLine("-------------------------------------------------");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Name = "Alex";
+            musteri1.Surname = "sj";
+
+            MusteriManager musteriManager=new MusteriManager();
+            musteriManager.musteriEkle(musteri1);
+            musteriManager.musteriListele(musteri1);
+            musteriManager.musteriSil(musteri1);
+        }
+    }
+}
